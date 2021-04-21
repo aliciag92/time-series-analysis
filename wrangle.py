80966472eea16e6b1e44f9754aa6277c9f607785
Alicia Gonzalez <alicia.j.gonzalez01@gmail.com>
2021-04-20 21:38:42 -0500

    need modeling and conclusion

--- wrangle.py
+++ wrangle.py
@@ -1,60 +1,89 @@
 import pandas as pd
 import numpy as numpy
 
-from sklearn.model_selection import train_test_split
-import sklearn.preprocessing
+from datetime import datetime
 
-############################# Acquire Climate Data ############################# 
+############################# Acquire Temperature Data ############################# 
 
-def get_movies_data():
+def get_temps_data():
     '''
     This function will pull the dataset from the provided .csv file 
     and will write it to a pandas dataframe. 
     
-    To reproduce the results, you will need the movies_metadata.csv file in your working directory.
+    To reproduce the results, you will need the GlobalLandTemperaturesByState.csv file downloaded in your working directory.
     '''
-    df = pd.read_csv('dataset.csv')
+    df = pd.read_csv('GlobalLandTemperaturesByState.csv')
     
     return df
 
 
 
 
-############################# Prepare Climate Data ############################# 
+############################# Prepare Temperature Data ############################# 
 
 # defines function to clean movies data and return as a cleaned pandas DataFrame
-def clean_climate(df):
+def clean_temps(df):
     '''
-    clean_climate will take one argument df, a pandas dataframe and will:
-    ___,
-    ____,
-    ___,
-    and ___
+    clean_temps will take one argument df, a pandas dataframe and will:
+    shorten dataset with only Texas temperatures,
+    rename columns,
+    convert date (`dt`) column to datetime datetype,
+    set date as index,
+    and drop `Country` and `State` columns
    
     return: a single pandas dataframe with the above operations performed
     '''
   
-    #___
-    df = 
+    #specify Texas data
+    df = df[df.State.str.contains('Texas')]
 
-    #
-    df = 
+    #rename columns
+    df = df.rename(columns={"dt": "date", 
+                        "AverageTemperature": "avg_temp",
+                        "AverageTemperatureUncertainty": "avg_uncertainty"})
 
+    #convert date column to datetime
+    df.date = pd.to_datetime(df.date)
 
-    #fill in values w/ very little nulls
-    df = df.fillna(0)
+    # setting date column to index
+    df = df.set_index('date').sort_index()
 
+    #drop `Country` column
+    df = df.drop(columns=['Country', 'State'])
 
     return df
 
 
 
-# splits a dataframe into train, validate, test 
+# splits a time series dataframe into train, validate, test 
 def split(df):
     '''
-    take in a DataFrame and return train, validate, and test DataFrames.
-    return train, validate, test DataFrames.
+    takes in a DataFrame,
+    sets train, validate, and test size, 
+    uses the values to split the DataFrame, and 
+
+    returns train, validate, test DataFrames.
     '''
-    #split w/ time series data
+
+    #set train size to be 50% of total
+    train_size = int(len(df) * .5)
+    train_size
+
+    #set validate size to be 30% of total
+    validate_size = int(len(df) * .3)
+    validate_size
+
+    #validate end index will go from 1162 to 1162 + 697
+    validate_end_index = train_size + validate_size
+    validate_end_index
+
+    #train will go from 0 to 1161
+    train = df[: train_size]
+
+    #validate will go from 1162 to 1858
+    validate = df[train_size:validate_end_index]
+
+    #test will include 1859 to the end
+    test = df[validate_end_index:]
 
     return train, validate, test
